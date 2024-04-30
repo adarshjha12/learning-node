6411641846e22ff446bb0f78163d010c49cd065e
adarsh <jhaa50872@gmail.com>
2024-04-30 22:00:07 +0530

    welcome

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let file = require('fs')
 file.writeFileSync('read.txt', 'welcome to node js')
 
 file.appendFileSync('read.txt', ' adarsh jha')
+
+let abc = file.readFileSync('read.txt')
+console.log(abc.toString());
+
+let xyz = file.renameSync('read.txt', 'readWrite.txt')
