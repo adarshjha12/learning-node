c98f32728b350c6887b14d18b5e2700c6a820c99
adarsh <jhaa50872@gmail.com>
2024-05-02 21:48:32 +0530

    modified

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let file = require('fs')
-file.writeFileSync('read.txt', 'welcome to node js')
+// file.writeFileSync('read.txt', 'welcome to node js')
 
-file.appendFileSync('read.txt', ' adarsh jha')
+// file.appendFileSync('read.txt', ' adarsh jha')// to update content in file
 
-let abc = file.readFileSync('read.txt')
-console.log(abc.toString());
+// let abc = file.readFileSync('read.txt', 'utf-8')  // to read content in file
 
-let xyz = file.renameSync('read.txt', 'readWrite.txt')
+// console.log(abc.toString());
+
+//let xyz = file.renameSync('.read.txt', '.abc.txt') // to rename title of file
+
+//file.unlinkSync('read.txt')   to delete file
+
+//file.mkdirSync('anyFolder')  //to create folder
+//file.rmdirSync('anyFolder')  // to delete folder
+
+// 🤓 these are examples of crud operations
+
+// file.writeFileSync('a.txt', 'hello')
+
+// file.mkdirSync('ghj')
+// file.writeFileSync('ghj/abc.txt', 'hhhhhhiiiiii')
+// file.renameSync('ghj/abc.txt', 'ghj/xyz.txt')
+// file.unlinkSync('ghj/xyz.txt')
+// file.rmdirSync('ghj')
