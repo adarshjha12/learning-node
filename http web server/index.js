66f634f37c240d5ffecae6a6588f1ac35f17f87d
adarsh <jhaa50872@gmail.com>
2024-05-08 19:49:20 +0530

    line

--- http web server/index.js
+++ http web server/index.js
@@ -1,8 +1,24 @@
+// routing------------
+// and web server
+
+
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.end('hello')
-    console.log(req.url);
+
+    if (req.url === '/'){
+        res.end('hello, i am home page')
+
+    } else if (req.url === '/contact'){
+        res.end('i am contact page')
+
+    }  else if (req.url === '/about'){
+        res.end('i am about page')
+    } else{
+        res.writeHead(404, { "content-type" : "text/html"});
+        res.end(" 404, not found ")
+
+    }
 })
 
 server.listen(8000, '127.0.0.1', () =>{
