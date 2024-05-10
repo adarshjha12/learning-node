f8a6857823bc23027251bc80bd9870851c897f19
adarsh <jhaa50872@gmail.com>
2024-05-10 15:14:43 +0530

    line

--- simple api/index.js
+++ simple api/index.js
@@ -6,8 +6,7 @@ const fs = require('fs')
 
 const server = http.createServer( (req, res) => {
 
-    // res.writeHead(200, {'content-type' : 'text/html'})
-    // res.end('server saying hello')
+
 
 if (req.url === '/') {
       res.writeHead(200, { 'content-type' : 'text/html'})
@@ -22,14 +21,11 @@ if (req.url === '/') {
 } else if (req.url === '/api') {
     fs.readFile(`api.json`, 'utf-8', (err, data) =>{
 
-        res.writeHead(200, { 'content-type' : 'text/JSON'})
-
-        // let objData = JSON.parse(data)
-        // res.end(JSON.parse(data))
-        // data.replace("\r\n", '');
         let jsonResponse = JSON.parse(data);
-        let modifiedResponse = JSON.stringify(jsonResponse);
-        res.end( modifiedResponse);
+        res.writeHead(200, { 'content-type' : 'application/json'})
+
+        let modifiedResponse = JSON.stringify(jsonResponse.results[0].gender);
+        res.end(modifiedResponse)
     })
 
 } else {
@@ -43,3 +39,42 @@ let port = 3000;
 server.listen(port, () =>{
     console.log(`server running on ${port}` );
 })
+
+// const http = require('http');
+// const fs = require('fs');
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.writeHead(200, { 'content-type': 'text/html' });
+//     res.end('server saying hello');
+//   } else if (req.url === '/about') {
+//     res.end('server saying about page');
+//   } else if (req.url === '/contact') {
+//     res.end('server saying contact page');
+//   } else if (req.url === '/api') {
+//     fs.readFile(`api.json`, 'utf-8', (err, data) => {
+//       if (err) {
+//         res.writeHead(500, { 'content-type': 'text/plain' });
+//         res.end('Internal Server Error');
+//         return;
+//       }
+
+//     //   try {
+//         let jsonResponse = JSON.parse(data);
+//         res.writeHead(200, { 'content-type': 'application/json' });
+//         res.end(JSON.stringify(jsonResponse.results[0].gender)); // Stringify the JSON data before sending
+//     //   } catch (error) {
+//     //     res.writeHead(500, { 'content-type': 'text/plain' });
+//     //     res.end('Error parsing JSON data');
+//     //   }
+//     });
+//   } else {
+//     res.writeHead(404, { 'content-type': 'text/html' });
+//     res.end('404 not found');
+//   }
+// });
+
+// let port = 3000;
+// server.listen(port, () => {
+//   console.log(`server running on ${port}`);
+// });
