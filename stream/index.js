df9f79bd6b1634f2a6b6293643cc87617526ad07
adarsh <jhaa50872@gmail.com>
2024-05-13 20:28:22 +0530

    line

--- stream/index.js
+++ stream/index.js
@@ -6,24 +6,40 @@ const fs = require('fs')
 
 let server = http.createServer()
 
+// reading data
+
 server.on('request', (req, res ) =>{
 
     let stream = fs.createReadStream('abc.txt')
-
-    stream.on('data', (chunkData) =>{
-        res.write(chunkData)
+     stream.on('data', (chunkData) =>{
+         res.write(chunkData)
     })
 
     stream.on('end', () =>{
-        res.end()
-    })
+         res.end()
+     })
 
-    stream.on('error', (err) =>{
-        console.log(err);
-        res.end('file not found')
+     stream.on('error', (err) =>{
+         console.log(err);
+         res.end('file not found')
+     })
+
+})
+
+// writing data
+server.on('request', (req, res ) =>{
+
+    let stream = fs.createWriteStream('write.txt')
+
+    stream.write('hello world')
+    stream.end()
+
+    stream.on('finish' , () =>{
+        res.end('Data has been written to the file');
     })
 
 })
+
 
 let port = 3000
 server.listen(port, () =>{
