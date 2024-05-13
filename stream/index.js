9387a82849e3c62a5208726cbd4c7f533ddeeaa9
adarsh <jhaa50872@gmail.com>
2024-05-13 19:19:47 +0530

    line

---
+++ stream/index.js
@@ -0,0 +1,31 @@
+// reading data with stream
+
+const http = require('http')
+const fs = require('fs')
+
+
+let server = http.createServer()
+
+server.on('request', (req, res ) =>{
+
+    let stream = fs.createReadStream('abc.txt')
+
+    stream.on('data', (chunkData) =>{
+        res.end(chunkData)
+    })
+
+    stream.on('end', () =>{
+        res.end()
+    })
+
+    stream.on('error', (err) =>{
+        console.log(err);
+        res.end('file not found')
+    })
+
+})
+
+let port = 3000
+server.listen(port, () =>{
+    console.log(`server is running on ${port}`);
+})
