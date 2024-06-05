441e8fc60cb353e487059f8980f79b2393058efb
adarsh <jhaa50872@gmail.com>
2024-06-05 19:49:42 +0530

    lines

--- project_1/index.js
+++ project_1/index.js
@@ -1,8 +1,17 @@
 let fs = require('fs')
 let http = require('http')
+let requests = require('requests')
 
-fs.readFileSync('index.html', 'utf-8')
+
+let file = fs.readFileSync('index.html', 'utf-8')
 
 let server = http.createServer( (req, res) =>{
 
+
+    if (req.url === '/') {
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=15facce67a51ac42cf416ee97a10bfc2
+
+        ")
+        .on('data', )
+    }
 })
