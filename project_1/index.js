5fa350609fc5a3c27cfbf26a9cdff2466111cab0
adarsh <jhaa50872@gmail.com>
2024-06-06 20:05:06 +0530

    line

--- project_1/index.js
+++ project_1/index.js
@@ -9,9 +9,17 @@ let server = http.createServer( (req, res) =>{
 
 
     if (req.url === '/') {
-        requests("https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=15facce67a51ac42cf416ee97a10bfc2
-
-        ")
-        .on('data', )
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Patna&appid=15facce67a51ac42cf416ee97a10bfc2"), {streaming}
+        .on('data', (chunk) =>{
+            console.log(chunk);
+        })
+        .on('end', (err) =>{
+           if (err) return console.log('connection closed due to error', err);
+        })
     }
+})
+
+let port = 3000
+server.listen(port, () =>{
+    console.log(`server is running on ${port}`);
 })
