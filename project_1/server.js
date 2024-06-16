365b1ad89b2f45600b9737ba344c58a8403b4a59
adarsh <jhaa50872@gmail.com>
2024-06-16 22:26:14 +0530

    line

--- project_1/server.js
+++ project_1/server.js
@@ -1,5 +1,15 @@
 let fs = require('fs')
 let http = require('http')
-let fetch = require('node-fetch')
+let axios = require('axios')
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15facce67a51ac42cf416ee97a10bfc2`
 
+let file = fs.readFileSync('index.html')
+
+let getWeather = async () =>{
+
+    try {
+        let response = await axios.get(url)
+    } catch (error) {
+        console.log('unable to fetch weather', error);
+    }
+}
