9cc51dfe0c9dace2432c0912457dd7654c43ba8f
adarsh <jhaa50872@gmail.com>
2024-06-18 15:51:01 +0530

    line

--- project_1/server.js
+++ project_1/server.js
@@ -1,9 +1,14 @@
 let fs = require('fs')
 let http = require('http')
-let axios = require('axios')
+let axios = require('axios');
+let urlModule = require('url');
+
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15facce67a51ac42cf416ee97a10bfc2`
 
+console.log(document);
 let file = fs.readFileSync('index.html')
+let text = document.getElementById('text')
+
 
 let getWeather = async () =>{
 
