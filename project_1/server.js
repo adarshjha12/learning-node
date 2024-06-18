ec9911ad9194a625e1743ea36a06c422d894d58e
adarsh <jhaa50872@gmail.com>
2024-06-18 22:40:20 +0530

    line

--- project_1/server.js
+++ project_1/server.js
@@ -3,7 +3,7 @@ let http = require('http')
 let axios = require('axios');
 let url = require('url');
 
-let link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15facce67a51ac42cf416ee97a10bfc2`
+let link = `https://api.openweathermap.org/data/2.5/weather?q=patna&appid=15facce67a51ac42cf416ee97a10bfc2`
 
 let file = fs.readFileSync('index.html')
 
@@ -12,7 +12,11 @@ let getWeather = async () =>{
 
     try {
         let response = await axios.get(link)
+        return response.data
     } catch (error) {
         console.log('unable to fetch weather', error);
     }
 }
+
+
+let server = http.createServer( async ())
--- project_1/test.js
+++ project_1/test.js
@@ -1,10 +1,29 @@
-const url = require('url');
-const querystring = require('querystring');
+// const url = require('url');
+// const querystring = require('querystring');
 
-const myURL = 'http://localhost:3000/weather?city=Patna&units=metric';
+// const myURL = 'http://localhost:3000/weather?city=Patna&units=metric';
 
-const parsedURL = url.parse(myURL, true).query;
-// url.parse(req.url, true).query;
-// const parsedQuery = querystring.parse(parsedURL.query);
+// const parsedURL = url.parse(myURL, true).query;
+// // url.parse(req.url, true).query;
+// // const parsedQuery = querystring.parse(parsedURL.query);
 
-console.log(parsedURL);
+// console.log(parsedURL);
+
+
+// fetch(`https:api.openweathermap.org/data/2.5/weather?q=patna&appid=15facce67a51ac42cf416ee97a10bfc2`
+// )
+// .then((Response) => console.log(Response))
+// .catch((Error) => console.log(Error));
+
+
+async function aaaaa () {
+    try {
+     let abvc = await fetch('https:api.openweathermap.org/data/2.5/weather?q=patna&appid=15facce67a51ac42cf416ee97a10bfc2')
+   let data = await abvc.json()
+   console.log(data);
+    } catch (error) {
+     console.log(error);
+    }
+ }
+
+ aaaaa()
