3ec0f284ec12a040b32242493574101258e5ed08
adarsh <jhaa50872@gmail.com>
2024-06-19 20:22:37 +0530

    line

--- project_1/server.js
+++ project_1/server.js
@@ -3,12 +3,12 @@ let http = require('http')
 let axios = require('axios');
 let url = require('url');
 
-let link = `https://api.openweathermap.org/data/2.5/weather?q=patna&appid=15facce67a51ac42cf416ee97a10bfc2`
+let link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=15facce67a51ac42cf416ee97a10bfc2`
 
 let file = fs.readFileSync('index.html')
 
 
-let getWeather = async () =>{
+let getWeather = async (city) =>{
 
     try {
         let response = await axios.get(link)
@@ -19,4 +19,18 @@ let getWeather = async () =>{
 }
 
 
-let server = http.createServer( async ())
+let server = http.createServer( async (req, res) =>{
+    if(req.url = '/'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.end(file)
+    } else if(req.url.startsWith('/weather')){
+        
+    }
+})
+
+
+
+let port = 3000
+server.listen(port, () =>{
+    console.log('server is running on :', port);
+})
