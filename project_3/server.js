eafb6851a404b2c11fea80fb53f8d91dc11be708
adarsh <jhaa50872@gmail.com>
2024-06-28 20:46:36 +0530

    fixes

--- project_3/server.js
+++ project_3/server.js
@@ -4,3 +4,15 @@ let url = require('url')
 let axios = require('axios')
 
 let file = fs.readFileSync('index.html', 'utf-8')
+
+let weatherAPI = '15facce67a51ac42cf416ee97a10bfc2';
+let apiKey = 'https://api.openweathermap.org/data/2.5/weather';
+
+let weather = async (city) =>{
+
+    try {
+        let response = await axios.get(`${weatherAPI}?q=${city}&appid=${apiKey}`)
+    } catch (error) {
+        console.log(error);
+    }
+}
