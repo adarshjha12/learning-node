4758b3db75c90826926202127c25e85187de1f01
adarsh <jhaa50872@gmail.com>
2024-06-28 23:20:24 +0530

    completed

--- project_3/server.js
+++ project_3/server.js
@@ -5,12 +5,13 @@ let axios = require('axios')
 
 let file = fs.readFileSync('index.html', 'utf-8')
 
-let weatherAPI = '15facce67a51ac42cf416ee97a10bfc2';
-let apiKey = 'https://api.openweathermap.org/data/2.5/weather';
+let weatherAPI = 'https://api.openweathermap.org/data/2.5/weather';
+let apiKey = '15facce67a51ac42cf416ee97a10bfc2';
 
 let weather = async (city) =>{
     try {
         let response = await axios.get(`${weatherAPI}?q=${city}&appid=${apiKey}`)
+        return response.data
     } catch (error) {
         console.log(error);
     }
@@ -18,8 +19,26 @@ let weather = async (city) =>{
 
 let server = http.createServer( async (req, res) =>{
     if (req.url === '/') {
-        res.writeHead(200, {'content-type' : 'text/html'})
         res.end(file)
+    } else if (req.url.startsWith('/weather')) {
+        let queryObject = url.parse(req.url, true).query
+        let city = queryObject.city
+
+        if (city) {
+            let weatherData = await weather(city)
+
+            if (weatherData) {
+               res.end(JSON.stringify(weatherData))
+               console.log(weatherData);
+            } else{
+                res.end('error fetching data');
+            }
+        } else{
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('enter city parameter')
+        }
+    } else{
+        res.end('not found')
     }
 })
 
